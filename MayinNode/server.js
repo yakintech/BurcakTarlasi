7b94d42bad3fa4b7bd8d17c6d6aefb166eb8531a
Çağatay YILDIZ <cagatay.yildiz@baogrenci.com>
2019-10-13 17:08:09 +0300

    server update

--- MayinNode/server.js
+++ MayinNode/server.js
@@ -5,43 +5,53 @@ var io = require('socket.io')(server);
 server.listen(3001, function () {
     console.log('listening on *:3001');
 });
-
+var x;
+var tutar;
 io.on('connection', function (socket) {
     var mayinlar = [];
-    socket.on('startgame', function (data) {
+    var oranlar = [];
+    socket.on('startgame', function (game) {
+        tutar = game.tutar;
         var satirmayin = [];
+        var oran = 1;
         for (i = 0; i < 10; i++) {
-            var mayinnumara1 = Math.floor(Math.random() * 4) + 1;
-            var mayinnumara2 = Math.floor(Math.random() * 4) + 1;
-
-            if (mayinnumara2 == mayinnumara1) {
-                i--;
+            for (j = 0; j < game.level; j++) {
+                var mayin = Math.floor(Math.random() * 4) + 1;
+                if (!satirmayin.includes(mayin)) {
+                    satirmayin.push(mayin);
+                }
+                else {
+                    j--
+                }
             }
-            else {
-                satirmayin.push(mayinnumara1);
-                satirmayin.push(mayinnumara2);
-
-                mayinlar.push(satirmayin);
-                satirmayin = [];
-            }
+            mayinlar.push(satirmayin);
+            satirmayin = [];
+            oran = oran * 1.17 *game.level;
+            oranlar.push(oran);
         }
         socket.emit("sendmayin", mayinlar);
-
+        socket.emit("oranlarigoster", oranlar);
     })
 
-    socket.on('oyna',function(oyundata){
-        var x =  oyundata.linenumber;
+    socket.on('oyna', function (oyundata) {
+        x = oyundata.linenumber;
         var tiklananmayin = Number(oyundata.tiklanan);
         var mayinvarmi = false;
-        for(i=0; i<2 ;i++){
-            if(mayinlar[x][i] == tiklananmayin){
+        for (i = 0; i < 2; i++) {
+            if (mayinlar[x][i] == tiklananmayin) {
                 mayinvarmi = true;
                 socket.emit('crash');
             }
         }
-        if(!mayinvarmi){
+        if (!mayinvarmi) {
             x++;
-            socket.emit('continue',x);
+            socket.emit('continue', x);
         }
+    })
+
+    socket.on('cek',function(){
+        var result = oranlar[x-2] * Number(tutar);
+        console.log(x + "- " + tutar);
+        socket.emit("endgame",result);
     })
 });
